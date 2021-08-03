913ae764160614ef9989cd9c60e1ecd3ebbcde8c
Andrew Jadot <andrew.jadot@gmail.com>
2021-08-03 15:57:54 -0400

    homepage is a thing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,43 @@
 <template>
   <div id="app">
+    <h1>mealTimE</h1>
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link id="home" v-bind:to="{ name: 'home' }">HOME </router-link >
+      <a href="#" id="recipe"> RECIPES </a> 
+      <a href="#" id="recipe"> MY RECIPES </a>
+      <a href="#" id="recipe"> MY MEAL PLAN </a> 
+      <a href="#" id="recipe"> GROCERY LIST </a> 
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''"> LOGOUT</router-link>
     </div>
     <router-view />
   </div>
 </template>
+<script>
+</script>
+
+<style>
+#app{
+  background-color: #2fcee1;
+  background-size: 100%;
+  font-family: Arial, Helvetica, sans-serif;
+}
+h1{
+  font-size: 40px;
+  padding-left: 10px;
+  padding-top: 10px;
+}
+#nav {
+  font-size: 18px;
+  height: 100%;
+  text-align: right;
+  border-bottom: 1px solid rgb(34, 88, 133);
+  padding-right: 10px;
+  padding-bottom:10px;
+}
+a{
+  margin-left: 2.5em;
+  text-decoration: none;
+  color: darkblue;
+  text-align: right;
+}
+</style>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -12,7 +12,7 @@
         role="alert"
         v-if="this.$route.query.registration"
       >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
+      <label for="username" class="sr-only">Username </label>
       <input
         type="text"
         id="username"
@@ -22,7 +22,7 @@
         required
         autofocus
       />
-      <label for="password" class="sr-only">Password</label>
+      <label for="password" class="sr-only"> Password </label>
       <input
         type="password"
         id="password"
@@ -74,3 +74,4 @@ export default {
   }
 };
 </script>
+
