03a917b826ee165ce4f879ef29ab567c2dc0a3c1
Ryan Godman <rgodman0038@gmail.com>
2021-08-04 12:18:27 -0400

    card implemented in all recipes

--- frontend/src/components/AllRecipesList.vue
+++ frontend/src/components/AllRecipesList.vue
@@ -1,10 +1,19 @@
 <template>
+<div>
+  <h2>All Recipes</h2>
   <div class="all-recipes-list">
-    <h2>All Recipes</h2>
     <div v-for="recipe in recipes" v-bind:key="recipe.recipeId" class="recipe">
-      <router-link v-bind:to="{ name: 'recipe', params: { recipeID: recipe.recipeId } }">
-        {{ recipe.recipeName }}
-      </router-link>
+      <div class="card">
+        <img v-bind:src="recipe.recipeImg" class="recipe-image" style="width:100%">
+          <h4><b>{{recipe.recipeName}}</b></h4>
+          <p class="card-text">{{ recipe.prepTime }} minutes prep time || {{ recipe.cookTime }} minutes cook time</p>
+          <router-link v-bind:to="{ name: 'recipe', params: { recipeID: recipe.recipeId } }">
+            <button>
+        {{ recipe.recipeName }} Recipe Details</button>
+        </router-link>
+      
+      </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,3 +35,42 @@ export default {
   }
 }
 </script>
+
+<style>
+
+.all-recipes-list {
+    background:white;
+    max-width: 300px;
+    margin: 30px auto;
+    padding: 0px;
+    border: 2px solid black;
+    border-radius: 5px;
+  
+  }
+.card {
+      padding: 10px;
+      border-width: 2px;
+      justify-content: center;
+  }
+
+  button {
+    cursor: pointer;
+    background-color:#2fcee1;
+    border: none;
+    color:black;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    
+  }
+  button:hover {background-color:orange}
+  
+  button:active {
+    background-color:orange;
+    box-shadow: 0 5px #666;
+    transform: translateY(4px);
+  }
+</style>
+
