ceaef7036a360e207f2ee057f17ccd7cb3b551df
Shannon Griffith <shannongriffith91@gmail.com>
2021-08-04 14:20:59 -0400

    Database 3 new recipes and imgs

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -101,9 +101,12 @@ CREATE TABLE meal_plan_user_recipes (
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
-INSERT INTO recipes (preparation, recipe_name, prep_time, cook_time, recipe_type) VALUES ('You will need fresh basil, fresh mozz, and fresh tomato on a plate. Layer the cheese, basil, and tomato. Drizzle balsamic vinegar and Enjoy!', 'Caprese Salad', 10, null, 'Salad');
-INSERT INTO recipes (preparation, recipe_name, prep_time, cook_time, recipe_type, recipe_img) VALUES ('Prepare the tortilini, drain. Cook the meat, then add the sauce and cream cheese. Put the tortellini into the backing dish and pour the meat/cheese/sauce mixture on top. Sprinkle the parmesan cheese over top. Bake for 20 min.', 'Cheesey Tortellini Pasta Bake', 15, 20, 'Entree', 'https://www.number-2-pencil.com/wp-content/uploads/2020/03/Baked-Tortellini-Casserole-5-1024x683.jpg');
-INSERT INTO recipes (preparation, recipe_name, prep_time, cook_time, recipe_type) VALUES('Thaw tater tots, add them to greased muffin pan, dividing evenly. Smash them, fill each cup with cooked meat and chopped veggies(you can use onions, mushrooms, and/or peppers, to name a few). Scramble eggs, add seasoning of your choosing, then pour eggs into each cup, evenly. Top with cheese. Bake at 350 degrees for 18-20 minutes.', 'Mini Omelets', 10, 20, 'Breakfast');
+INSERT INTO recipes (preparation, recipe_name, prep_time, cook_time, recipe_type, recipe_img) VALUES ('You will need fresh basil, fresh mozz, and fresh tomato on a plate. Layer the cheese, basil, and tomato. Drizzle balsamic vinegar and Enjoy!', 'Caprese Salad', 10, null, 'Salad', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2019/07/Caprese-Salad-4.jpg');
+INSERT INTO recipes (preparation, recipe_name, prep_time, cook_time, recipe_type) VALUES ('Prepare the tortilini, drain. Cook the meat, then add the sauce and cream cheese. Put the tortellini into the backing dish and pour the meat/cheese/sauce mixture on top. Sprinkle the parmesan cheese over top. Bake for 20 min.', 'Cheesey Tortellini Pasta Bake', 15, 20, 'Entree');
+INSERT INTO recipes (preparation, recipe_name, prep_time, cook_time, recipe_type, recipe_img) VALUES('Thaw tater tots, add them to greased muffin pan, dividing evenly. Smash them, fill each cup with cooked meat and chopped veggies(you can use onions, mushrooms, and/or peppers, to name a few). Scramble eggs, add seasoning of your choosing, then pour eggs into each cup, evenly. Top with cheese. Bake at 350 degrees for 18-20 minutes.', 'Mini Omelets', 10, 20, 'Breakfast', 'https://www.skinnytaste.com/wp-content/uploads/2016/02/loadedbakedeggomeletmuffins-7-550x825.jpg');
+INSERT INTO recipes (preparation, recipe_name, prep_time, cook_time, recipe_type, recipe_img) VALUES('Place raw chicken in the slow cooker.  Layer raw potatoes and carrots on top. In a small sauce pan, melt butter.  Add garlic, salt, pepper, dried thyme, and dried parsley. Pour mixture over the chicken and potato/veggies. Cook on high for 4 hours.', 'Slow Cooker Garlic Butter Chicken', 5, 240, 'Entree', 'https://www.themagicalslowcooker.com/wp-content/uploads/2017/02/garlic-butter-chicken-and-veggies-2-2.jpg.webp');
+INSERT INTO recipes (preparation, recipe_name, prep_time, cook_time, recipe_type, recipe_img) VALUES('Add ingredients to the blender. Blend for 45 seconds and Enjoy!', 'Skinny Wendys Frosty', 4, null, 'Dessert', 'http://4.bp.blogspot.com/-rnSN27O0AEg/UcjFUQjzZUI/AAAAAAAAAdA/IapK8R-AYtk/s640/IMG_2084_edited-1.jpg');
+INSERT INTO recipes (preparation, recipe_name, prep_time, cook_time, recipe_type, recipe_img) VALUES('Cut the cinnamon rolls into quarters, and toss them with the apple pie filling. Spread the mixture onto the bottom of a small greased baking dish and bake for 25 minutes in a 375 degree oven.', 'Easy Cinnamon Rolle Apple Cobbler', 3, 25, 'Dessert', 'https://www.thelazydish.com/wp-content/uploads/2020/09/easy-apple-cobbler-recipe.jpg');
 
 INSERT INTO ingredients (ingredient_name) VALUES ('Mozzerella Cheese');
 INSERT INTO ingredients (ingredient_name) VALUES ('Basil');
@@ -121,6 +124,23 @@ INSERT INTO ingredients (ingredient_name) VALUES ('Cooked Sausage');
 INSERT INTO ingredients (ingredient_name) VALUES ('Frozen Tater Tots');
 INSERT INTO ingredients (ingredient_name) VALUES ('Eggs');
 INSERT INTO ingredients (ingredient_name) VALUES ('Shredded Cheese');
+
+INSERT INTO ingredients (ingredient_name) VALUES ('Boneless Chicken Breasts');
+INSERT INTO ingredients (ingredient_name) VALUES ('Baby Carrots');
+INSERT INTO ingredients (ingredient_name) VALUES ('Yukon Gold Potatoes');
+INSERT INTO ingredients (ingredient_name) VALUES ('Salted Butter');
+INSERT INTO ingredients (ingredient_name) VALUES ('Minced Garlic');
+INSERT INTO ingredients (ingredient_name) VALUES ('Dried Thyme');
+INSERT INTO ingredients (ingredient_name) VALUES ('Dried Parsley');
+
+INSERT INTO ingredients (ingredient_name) VALUES ('Almond Milk');
+INSERT INTO ingredients (ingredient_name) VALUES ('Ice Cubes');
+INSERT INTO ingredients (ingredient_name) VALUES ('Vanilla');
+INSERT INTO ingredients (ingredient_name) VALUES ('Unsweetened Cocoa Powder');
+INSERT INTO ingredients (ingredient_name) VALUES ('Banana');
+
+INSERT INTO ingredients (ingredient_name) VALUES ('Can of Apple Pie Filling');
+INSERT INTO ingredients (ingredient_name) VALUES ('Pillsbury Cinnamon Rolls');
 
 INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES (1, 1, 'cup', 1);
 INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES (2,  1, 'tablespoon', 3);
@@ -140,8 +160,22 @@ INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, meas
 INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(13, 3, 'egg', 10);
 INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(14, 3, 'cup', 2);
 
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(15, 4, 'lb', 1);
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(16, 4, 'lb', 1);
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(17, 4, 'lb', 2);
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(18, 4, 'cup', 1);
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(19, 4, 'tablespoon', 1);
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(20, 4, 'teaspoon', 1);
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(21, 4, 'teaspoon', 1);
 
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(22, 5, 'cup', 1);
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(23, 5, 'ice cubes', 15);
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(24, 5, 'teaspoon', 1);
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(25, 5, 'tablespoon', 1);
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(26, 5, 'banana', 1);
 
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(27, 6, 'oz', 20);
+INSERT INTO recipe_ingredients (ingredient_id, recipe_id, measurement_unit, measurement_amount) VALUES(28, 6, 'tube', 1);
 
 
 INSERT INTO user_meal_plan (meal_plan_name) VALUES ('Healthy');
