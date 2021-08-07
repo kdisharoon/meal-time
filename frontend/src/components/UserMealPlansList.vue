66c8c63061b47b9bf11ff3755d7486ebd72c5b17
Ryan Godman <rgodman0038@gmail.com>
2021-08-07 16:43:54 -0400

    meal plan form - in progress

--- frontend/src/components/UserMealPlansList.vue
+++ frontend/src/components/UserMealPlansList.vue
@@ -4,16 +4,36 @@
     <div class="user-meal-plans-list d-flex justify-content-around">
       <div v-for="mealPlan in mealPlans" v-bind:key="mealPlan.mealPlanId" class="meal-plan">
         <div class="card">
-          <router-link v-bind:to="{ name: 'meal-plan', params: { userID: mealPlan.userId, mealPlanID: mealPlan.mealPlanId } }">
-            <button class="add-a-class-here-to-style-this-button-please-and-thank-you">
+          <router-link class="userMealPlanButon" v-bind:to="{ name: 'meal-plan', params: { userID: mealPlan.userId, mealPlanID: mealPlan.mealPlanId } }">
+            <button>
               {{ mealPlan.mealPlanName }}
             </button>
           </router-link>
         </div>
       </div>
-      <button class="add-a-class-here-to-style-this-button-please-and-thank-you" v-on:click.prevent="addMealPlan">
+      <div class="clearfix">
+      <button id="btnToggleForm" type="button" class="btn" v-on:click.prevent="addMealPlan">
           Add a Meal Plan
       </button>
+      </div>
+      
+
+<div class="container mealPlanStyle">
+  <form id="planForm" action="/action_page.php">
+  <div class="card">
+    <div class="col-25">
+      <label for="fname">Meal Plan Name</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="mname" name="mealplanname" placeholder="Plan name..">
+    </div>
+  </div>
+  <div class="row">
+    <input class="btn" type="submit" value="Create Meal Plan">
+  </div>
+  </form>
+</div>
+
 
     </div>
   </div>
@@ -50,6 +70,66 @@ export default {
     mealPlanService.getAllUserMealPlans(this.$route.params.userID).then(response => {
       this.mealPlans = response.data;
     });
-  }
+  },
 }
 </script>
+
+<style>
+
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+}
+
+label {
+  padding: 12px 12px 12px 0;
+  text-align: center;
+}
+
+/* input[type=submit] {
+  background-color: orange;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  float: right;
+}
+
+input[type=submit]:hover {
+  background-color: orange;
+} */
+
+.mealPlanStyle {
+  max-width: 300px;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+  border: solid black 2px;
+}
+
+
+
+.col-75 {
+  float: left;
+  width: 75%;
+  margin-top: 6px;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+@media screen and (max-width: 600px) {
+  .col-25, .col-75, input[type=submit] {
+    width: 100%;
+    margin-top: 0;
+  }
+}
+</style>
