be011a77e3ff351b904c62fe074266aec1bfdf25
Ezra Saywitz <ezra.saywitz@gmail.com>
2021-08-12 09:31:02 -0400

    added principal to the endpoints

--- backend/src/main/java/com/techelevator/controller/MealPlanController.java
+++ backend/src/main/java/com/techelevator/controller/MealPlanController.java
@@ -26,7 +26,7 @@ public class MealPlanController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(path = "/users/mealplans", method = RequestMethod.POST)
-    public MealPlan addMealPlanToUser(@RequestBody MealPlan mealPlan){
+    public MealPlan addMealPlanToUser(@RequestBody MealPlan mealPlan, Principal principal){
 
         try{
             return mealPlanDao.createMealPlan(mealPlan);
@@ -37,7 +37,7 @@ public class MealPlanController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(path = "/users/{userId}/mealplans/recipes/{recipeId}", method = RequestMethod.POST)
-    public void addRecipeToUserMealPlan(@PathVariable long userId, @PathVariable long recipeId, @RequestBody OrganizedRecipe organizedRecipe){
+    public void addRecipeToUserMealPlan(@PathVariable long userId, @PathVariable long recipeId, @RequestBody OrganizedRecipe organizedRecipe, Principal principal){
         mealPlanDao.addRecipeToUserMealPlan(userId, recipeId, organizedRecipe);
 
     }
@@ -59,19 +59,19 @@ public class MealPlanController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @RequestMapping(path= "/users/{userId}/mealplans/recipes/{recipeId}", method = RequestMethod.DELETE)
-    public void deleteRecipeFromMealPlan(@PathVariable long userId, @PathVariable long recipeId){
+    public void deleteRecipeFromMealPlan(@PathVariable long userId, @PathVariable long recipeId, Principal principal){
         mealPlanDao.deleteRecipeFromMealPlan(userId,recipeId);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(path = "/users/{userId}/grocerylist", method = RequestMethod.GET)
-    public Ingredient[] groceryList(@PathVariable long userId){
+    public Ingredient[] groceryList(@PathVariable long userId, Principal principal){
         return mealPlanDao.groceryList(userId);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @RequestMapping(path = "/users/{userId}/mealplans", method = RequestMethod.PUT)
-    public void updateMealPlanName(@PathVariable long userId, @RequestParam String mealPlanName){
+    public void updateMealPlanName(@PathVariable long userId, @RequestParam String mealPlanName, Principal principal){
         mealPlanDao.updateMealPlanName(userId, mealPlanName);
 
     }
--- backend/src/main/java/com/techelevator/model/RecipeList.java
+++ backend/src/main/java/com/techelevator/model/RecipeList.java
@@ -12,21 +12,21 @@ public class RecipeList {
 
     public RecipeList(){
         rIds= new ArrayList<>();
+        rNames = new ArrayList<>();
     }
 
     public void addToList(long id){
         rIds.add(id);
+
     }
 
     public long[] getArray(){
         Long[] s = rIds.toArray(new Long[0]);
         long[] a = new long[s.length];
         for (int i = 0; i< s.length; i++){
-
             a[i]= s[i];
         }
         return a;
-
     }
 
     public void addToList(String name){
