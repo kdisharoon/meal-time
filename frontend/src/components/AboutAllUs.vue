32e00015e417f34e063911cea029d04dd098071e
Ryan Godman <rgodman0038@gmail.com>
2021-08-12 12:39:44 -0400

    about us

---
+++ frontend/src/components/AboutAllUs.vue
@@ -0,0 +1,312 @@
+<template>
+
+                        
+ <div class="container bootstrap snippets bootdey">
+    <div class="profile person">
+        <div class="profile-body">
+            <div class="profile-bio">
+                <div class="row">
+                    <div class="col-md-5 text-center">
+                        <img class="img-thumbnail md-margin-bottom-10" src="https://bootdey.com/img/Content/user-453533-fdadfd.png" alt="">
+                    </div>
+                    <div class="col-md-7">
+                        <h2>Edward Rooster</h2>
+                        <span><strong>Job:</strong> Web Developer</span>
+                        <span><strong>Position:</strong> Web Designer</span>
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget massa nec turpis congue bibendum. Integer nulla felis, porta suscipit nulla et, dignissim commodo nunc. Morbi a semper nulla.</p>
+                        <p>Proin mauris odio, pharetra quis ligula non, vulputate vehicula quam. Nunc in libero vitae nunc ultricies tincidunt ut sed leo. Sed luctus dui ut congue consequat. Cras consequat nisl ante, nec malesuada velit pellentesque ac. Pellentesque nec arcu in ipsum iaculis convallis.</p>
+                    </div>
+                </div>    
+            </div>
+    </div>
+    </div>
+</div>                                                            
+
+<div class="container bootstrap snippets bootdey">
+    <div class="profile person">
+        <div class="profile-body">
+            <div class="profile-bio">
+                <div class="row">
+                    <div class="col-md-5 text-center">
+                        <img class="img-thumbnail md-margin-bottom-10" src="https://bootdey.com/img/Content/user-453533-fdadfd.png" alt="">
+                    </div>
+                    <div class="col-md-7">
+                        <h2>Edward Rooster</h2>
+                        <span><strong>Job:</strong> Web Developer</span>
+                        <span><strong>Position:</strong> Web Designer</span>
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget massa nec turpis congue bibendum. Integer nulla felis, porta suscipit nulla et, dignissim commodo nunc. Morbi a semper nulla.</p>
+                        <p>Proin mauris odio, pharetra quis ligula non, vulputate vehicula quam. Nunc in libero vitae nunc ultricies tincidunt ut sed leo. Sed luctus dui ut congue consequat. Cras consequat nisl ante, nec malesuada velit pellentesque ac. Pellentesque nec arcu in ipsum iaculis convallis.</p>
+                    </div>
+                </div>    
+            </div>
+    </div>
+    </div>
+</div> 
+
+<div class="container bootstrap snippets bootdey">
+    <div class="profile person">
+        <div class="profile-body">
+            <div class="profile-bio">
+                <div class="row">
+                    <div class="col-md-5 text-center">
+                        <img class="img-thumbnail md-margin-bottom-10" src="https://bootdey.com/img/Content/user-453533-fdadfd.png" alt="">
+                    </div>
+                    <div class="col-md-7">
+                        <h2>Edward Rooster</h2>
+                        <span><strong>Job:</strong> Web Developer</span>
+                        <span><strong>Position:</strong> Web Designer</span>
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget massa nec turpis congue bibendum. Integer nulla felis, porta suscipit nulla et, dignissim commodo nunc. Morbi a semper nulla.</p>
+                        <p>Proin mauris odio, pharetra quis ligula non, vulputate vehicula quam. Nunc in libero vitae nunc ultricies tincidunt ut sed leo. Sed luctus dui ut congue consequat. Cras consequat nisl ante, nec malesuada velit pellentesque ac. Pellentesque nec arcu in ipsum iaculis convallis.</p>
+                    </div>
+                </div>    
+            </div>
+    </div>
+    </div>
+</div> 
+
+<div class="container bootstrap snippets bootdey">
+    <div class="profile person">
+        <div class="profile-body">
+            <div class="profile-bio">
+                <div class="row">
+                    <div class="col-md-5 text-center">
+                        <img class="img-thumbnail md-margin-bottom-10" src="https://bootdey.com/img/Content/user-453533-fdadfd.png" alt="">
+                    </div>
+                    <div class="col-md-7">
+                        <h2>Edward Rooster</h2>
+                        <span><strong>Job:</strong> Web Developer</span>
+                        <span><strong>Position:</strong> Web Designer</span>
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget massa nec turpis congue bibendum. Integer nulla felis, porta suscipit nulla et, dignissim commodo nunc. Morbi a semper nulla.</p>
+                        <p>Proin mauris odio, pharetra quis ligula non, vulputate vehicula quam. Nunc in libero vitae nunc ultricies tincidunt ut sed leo. Sed luctus dui ut congue consequat. Cras consequat nisl ante, nec malesuada velit pellentesque ac. Pellentesque nec arcu in ipsum iaculis convallis.</p>
+                    </div>
+                </div>    
+            </div>
+    </div>
+    </div>
+</div> 
+
+<div class="container bootstrap snippets bootdey">
+    <div class="profile person">
+        <div class="profile-body">
+            <div class="profile-bio">
+                <div class="row">
+                    <div class="col-md-5 text-center">
+                        <img class="img-thumbnail md-margin-bottom-10" src="https://bootdey.com/img/Content/user-453533-fdadfd.png" alt="">
+                    </div>
+                    <div class="col-md-7">
+                        <h2>Edward Rooster</h2>
+                        <span><strong>Job:</strong> Web Developer</span>
+                        <span><strong>Position:</strong> Web Designer</span>
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eget massa nec turpis congue bibendum. Integer nulla felis, porta suscipit nulla et, dignissim commodo nunc. Morbi a semper nulla.</p>
+                        <p>Proin mauris odio, pharetra quis ligula non, vulputate vehicula quam. Nunc in libero vitae nunc ultricies tincidunt ut sed leo. Sed luctus dui ut congue consequat. Cras consequat nisl ante, nec malesuada velit pellentesque ac. Pellentesque nec arcu in ipsum iaculis convallis.</p>
+                    </div>
+                </div>    
+            </div>
+    </div>
+    </div>
+</div> 
+
+
+
+  <!-- <div class = "about_container">
+    <header id="header" class = "head">
+        <h1>The Developers of </h1>
+        <h1>meal TimE</h1>
+    </header>
+    <div class="bio" id="shay">
+      <img src="../assets/shay.jpg" />
+      <div class="about">
+          <h1>Shay Griffith</h1>
+        <p>
+          FILLER
+        </p>
+      </div>
+    </div>
+    <div class="bio" id="ezra">
+      <img src="../assets/ezra.jpeg" />
+      <div class="about">
+          <h1>Ezra Saywitz</h1>
+        <p>
+          FILLER
+        </p>
+      </div>
+    </div>
+    <div class="bio" id="aj">
+      <img src="../assets/aj.jpeg" />
+      <div class="about">
+          <h1>AJ Jadot</h1>
+        <p>
+          FILLER
+        </p>
+      </div>
+    </div>
+    <div class="bio" id="kris">
+      <img src="../assets/kris.jpeg" />
+      <div class="about">
+          <h1>Kris Disharoon</h1>
+        <p>
+          FILLER
+        </p>
+      </div>
+    </div>
+    <div class="bio" id="ryan">
+      <img src="../assets/ryan.jpg" />
+      <div class="about">
+          <h1>Ryan Godman</h1>
+        <p>
+          FILLER
+        </p>
+      </div>
+    </div>
+    <footer id="footer">
+        <img class="" src="">
+    </footer>
+
+  </div> -->
+</template>
+
+<script>
+export default {
+name: 'about-all-us'
+}
+</script>
+
+
+<style>
+
+                        
+
+.person{
+    -moz-border-radius: 2%;
+    -webkit-border-radius: 2%;
+    border-radius: 2%;
+    box-shadow: 5px 5px 0 rgba(0,0,0,0.08);
+}
+
+.profile .profile-body {
+    padding: 20px;
+    background: #f7f7f7;
+}
+
+.profile .profile-bio {
+    background: #fff;
+    position: relative;
+    padding: 15px 10px 5px 15px;
+}
+
+.profile .profile-bio a {
+    left: 50%;
+    bottom: 20px;
+    margin: -62px;
+    text-align: center;
+    position: absolute;
+}
+
+.profile .profile-bio h2 {
+    margin-top: 0;
+    font-weight: 200;
+}
+
+h1, h2, h3, h4, h5, h6 {
+    color: #585f69;
+    margin-top: 5px;
+    text-shadow: none;
+    font-weight: normal;
+    font-family: 'Open Sans', sans-serif;
+}
+h2 {
+    font-size: 24px;
+    line-height: 33px;
+}
+
+p, li, li a {
+    color: #555;
+}                                                            
+
+
+
+/*html {
+    background-color: #7DDFA4;
+}
+.head {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 0px;
+}
+.logo {
+    max-width: 500px;
+  }
+.ab{
+  background-color: #7DDFA4;
+  }
+.about_container {
+    margin: 10px;
+    padding-left: 65px;
+    padding-right: 65px;
+    font-family: sans-serif;
+    display: grid;
+    grid-template-columns: 2fr 2fr;
+    grid-gap: 30px;
+    grid-template-areas:
+        "header header"
+        "shay ezra"
+        "aj kris"
+        "ryan";
+  }
+#header {
+    grid-area: header; 
+}
+#shay {
+    grid-area: shay;
+}
+#ezra {
+    grid-area: ezra;
+}
+#aj {
+    grid-area: aj;
+}
+#kris {
+    grid-area: kris;
+}
+#ryan {
+    grid-area: ryan;
+}
+#footer {
+    grid-area: footer;
+}
+  .bio {
+    background-color: #F0F0F0;
+    padding: 10px;
+    border: solid 2px rgb(3, 3, 3);
+    display: flex;
+    align-items: center;
+    
+  }
+  .bio img {
+    max-height: 200px;
+    max-width: 200px;
+    border-radius: 800px;
+    margin: 10px;
+  }
+  .bio:nth-child(1) img {
+    order: 2;
+  }
+ #footer {
+     display: flex;
+     justify-content: center;
+ } 
+.img {
+    max-width: 200px;
+}
+h1 {
+    line-height: 20px;
+}
+p {
+    line-height: 24px;
+}*/
+</style>
